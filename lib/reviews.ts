de44a613a2e38501d16cff48a8f2c395a4506777
Mahmoud Hasanzadeh <mahmoodh1371@gmail.com>
2024-01-28 22:36:59 +0330

    add next.config.js file, delete content folder, replace strapi cms and its api with local markdown files

--- lib/reviews.ts
+++ lib/reviews.ts
@@ -1,13 +1,22 @@
-import { readdir, readFile } from "node:fs/promises";
-import matter from "gray-matter";
 import { marked } from "marked";
+import qs from "qs";
+
+const CMS_URL = "http://localhost:1337";
+
+interface CmsItem {
+  id: number;
+  attributes: any;
+}
 
 export interface Review {
   title: string;
   date: string;
   image: string;
-  body: string;
   slug: string;
+}
+
+export interface FullReview extends Review {
+  body: string;
 }
 
 export async function getFeaturedReview(): Promise<Review> {
@@ -15,30 +24,56 @@ export async function getFeaturedReview(): Promise<Review> {
   return reviews[0];
 }
 
-export async function getReview(slug: string): Promise<Review> {
-  const text = await readFile(`./content/reviews/${slug}.md`, "utf8");
-  const {
-    content,
-    data: { title, date, image },
-  } = matter(text);
-  const body = await marked(content);
-  return { slug, title, date, image, body };
+export async function getReview(slug: string): Promise<FullReview> {
+  const { data } = await fetchReviews({
+    filters: { slug: { $eq: slug } },
+    fields: ["slug", "title", "subtitle", "publishedAt", "body"],
+    populate: { image: { fields: ["url"] } },
+    pagination: { pageSize: 1, withCount: false },
+  });
+  const item = data[0];
+  return {
+    ...toReview(item),
+    body: await marked(item.attributes.body),
+  };
 }
 
 export async function getReviews(): Promise<Review[]> {
-  const slugs = await getSlugs();
-  const reviews: Review[] = [];
-  for (const slug of slugs) {
-    const review = await getReview(slug);
-    reviews.push(review);
-  }
-  reviews.sort((a, b) => b.date.localeCompare(a.date));
-  return reviews;
+  const { data } = await fetchReviews({
+    fields: ["slug", "title", "subtitle", "publishedAt"],
+    populate: { image: { fields: ["url"] } },
+    sort: ["publishedAt:desc"],
+    pagination: { pageSize: 6 },
+  });
+  return data.map(toReview);
 }
 
 export async function getSlugs(): Promise<string[]> {
-  const files = await readdir("./content/reviews");
-  return files
-    .filter((file) => file.endsWith(".md"))
-    .map((file) => file.slice(0, -".md".length));
+  const { data } = await fetchReviews({
+    fields: ["slug"],
+    sort: ["publishedAt:desc"],
+    pagination: { pageSize: 100 },
+  });
+  return data.map((item: CmsItem) => item.attributes.slug);
+}
+
+async function fetchReviews(parameters: any) {
+  const url =
+    `${CMS_URL}/api/reviews?` +
+    qs.stringify(parameters, { encodeValuesOnly: true });
+  const response = await fetch(url);
+  if (!response) {
+    throw new Error(`CMS returned ${response.status} for ${url}`);
+  }
+  return await response.json();
+}
+
+function toReview(item: CmsItem): Review {
+  const { attributes } = item;
+  return {
+    slug: attributes.slug,
+    title: attributes.title,
+    date: attributes.publishedAt.slice(0, "yyyy-mm-dd".length),
+    image: CMS_URL + attributes.image.data.attributes.url,
+  };
 }
--- scripts/strapi-request.mjs
+++ scripts/strapi-request.mjs
@@ -1,6 +1,18 @@
 import { writeFileSync } from "node:fs";
+import qs from "qs";
 
-const url = "http://localhost:1337/api/reviews" + "?populate=*";
+const url =
+  "http://localhost:1337/api/reviews" +
+  "?" +
+  qs.stringify(
+    {
+      filters: { slug: { $eq: "hades-2018" } },
+      fields: ["slug", "title", "subtitle", "publishedAt", "body"],
+      populate: { image: { fields: ["url"] } },
+      pagination: { pageSize: 1, withCount: false },
+    },
+    { encodeValuesOnly: true }
+  );
 const response = await fetch(url);
 const body = await response.json();
 const formatted = JSON.stringify(body, null, 2);
